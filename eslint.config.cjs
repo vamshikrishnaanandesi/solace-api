64d84be7ef8f21802a4b4a295d1accff3f2beb4d
Vamshi Krishna Anandesi <vamshikrishnaanandesi@Vamshis-MacBook-Air.local>
2025-11-06 11:09:22 +0530

    README Update

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,10 +1,13 @@
-const globals = require("globals");
-const tseslint = require("typescript-eslint");
-const { defineConfig } = require("eslint/config");
-const prettierConfig = require("eslint-config-prettier");
+const globals = require('globals');
+const tseslint = require('typescript-eslint');
+const { defineConfig } = require('eslint/config');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser },
+  },
   ...tseslint.configs.recommended,
   prettierConfig,
   {
